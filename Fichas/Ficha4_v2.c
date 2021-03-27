8e008ca77b597ec296090b26b949db62d0ebc418
IvoLims <75138193+IvoLims@users.noreply.github.com>
2021-03-27 20:05:31 +0000

    Update Ficha4_v2.c

--- Fichas/Ficha4_v2.c
+++ Fichas/Ficha4_v2.c
@@ -21,7 +21,9 @@ int main(){
     return 0;
 } 
 
-/* 2. Defina uma função int retiraVogaisRep (char *s) que remove de uma string todas as repetições consecutivas de vogais. A função deverá retornar o número de vogas removidas. Por exemplo, se a string a == "Estaa e umaa string coom duuuplicadoos" depois de invocarmos retiraVogaisRep a, a string a deverá ter o valor "Esta e uma string com duplicados". */
+/* 2. Defina uma função int retiraVogaisRep (char *s) que remove de uma string todas as repetições consecutivas de vogais.
+A função deverá retornar o número de vogas removidas. Por exemplo, se a string a == "Estaa e umaa string coom duuuplicadoos"
+depois de invocarmos retiraVogaisRep a, a string a deverá ter o valor "Esta e uma string com duplicados". */
 
 int retiraVogaisRep(char *s) {
     int i, j, last;
@@ -81,7 +83,9 @@ int main()
 	return 0;
 }
 
-/* 3. Defina uma função int duplicaVogais (char *s) que duplica todas as vogais de uma string. A função deve retornar o número de caracteres acrescentados. Assuma que o array recebido como argumento tem capacidade para armazenar o resultado pretendido. */
+/* 3. Defina uma função int duplicaVogais (char *s) que duplica todas as vogais de uma string. A função deve 
+retornar o número de caracteres acrescentados. Assuma que o array recebido como argumento tem capacidade para 
+armazenar o resultado pretendido. */
 
 int duplicaVogais (char *s){
     int res=0,j=0,i=0,count=0;
@@ -123,16 +127,70 @@ int main() {
     return 0;
 } 
 
-/* 5. Defina uma função void merge (int a[], int na, int b[], int nb, int r[]) que recebe dois arrays ordenados a e b (com tamanhos na e nb respectivamente) e os funde num só array ordenado r. Assuma que o array r tem capacidade para armazenar os na+nb elementos. */
+/* 5. Defina uma função void merge (int a[], int na, int b[], int nb, int r[]) que recebe dois arrays ordenados a e 
+b (com tamanhos na e nb respectivamente) e os funde num só array ordenado r. Assuma que o array r tem capacidade para 
+armazenar os na+nb elementos. */
 
 void merge (int a[], int na, int b[], int nb, int r[]){
-     
+     int i, j, k;
+     j = k = 0;
+     for (i = 0; i < na + nb;) {
+          if (j < na && k < nb) {
+              if (a[j] < b[k]) {
+                  r[i] = a[j], j++;
+              }
+              else { r[i] = b[k], k++;
+              }
+           i++;
+          } else if (j == na) {
+                     for (; i < na + nb;) { 
+                          r[i] = b[k], k++, i++;
+                     }
+            } else { 
+                    for (; i < na + nb;) {
+                         r[i] = a[j], j++, i++;
+                    }
+            }
+     }
 }
+
 
 int main() {
     int s1[15]={1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15},s2[4]={12,13, 14, 15}, r[19];
     merge(s1, 15, s2, 4, r);
+    printf("O array fica assim: ");
+    for(int i =0;i<19;i++){
+        printf("%d ", r[i]);
+    } printf("\n");
     return 0;
 }
 
-/*6. Defina uma função int partition (int v[], int N, int x) que, dado um array v de tamanho N e um inteiro x, reorganiza o array de forma a que come¸cam por aparecer todos os elementos menores ou iguais a x seguidos dos restantes elementos. A função retorna o número de elementos que ficaram na primeira parte do array (i.e., que são menores ou iguais a x). */
+/*6. Defina uma função int partition (int v[], int N, int x) que, dado um array v de tamanho N e um inteiro x, reorganiza 
+o array de forma a que começam por aparecer todos os elementos menores ou iguais a x seguidos dos restantes elementos. A 
+função retorna o número de elementos que ficaram na primeira parte do array (i.e., que são menores ou iguais a x). */
+
+int partition (int v[], int N, int x){
+    int res = 0,k=0,j=0;
+    int copy1[N],copy2[N];
+    for(int i=0;i<N;i++){
+        if(v[i]<=x){
+           copy1[j++]+=v[i],res++;
+        } else{copy2[k++]+=v[i];}
+    }
+    memcpy(v,copy1, res*sizeof(int)), k=0;
+    for(;k<N-res;k++){
+        v[j++]=copy2[k];
+    }
+    return res;
+}
+
+int main() {
+    int s[15]={1, 2, 3, 4, 5, 6, 4, 8, 9, 3, 11, 12, 3, 14, 15};
+    int x = 4;
+    printf("Ficaram na primeira parte %d elementos que são menores ou iguais a %d.\n",partition(s, 15, x),x);
+    printf("O array fica assim: ");
+    for(int i =0;i<15;i++){
+        printf("%d ", s[i]);
+    } printf("\n");
+    return 0;
+}
